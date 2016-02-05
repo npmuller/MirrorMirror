88b82d12951932c8810678c2c64a220a668fbe18
Nathan Muller <npmuller67@gmail.com>
2016-02-05 00:04:31 -0500

    Added more greetings.

--- config/mirror.php
+++ config/mirror.php
@@ -3,5 +3,7 @@
 return [
 	'greetings' => [
 		'You look especially amazing today.',
+                'You are just beautiful, you know that?',
+                'Who is that wonderful person looking at me?!'
 	]
 ];
