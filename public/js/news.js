90e47e87ce60ab56b62a13e8f8b06bd90341f308
Nathan Muller <npmuller67@gmail.com>
2017-06-01 22:56:13 -0400

    re did news api to work with newsapi.org

--- public/js/news.js
+++ public/js/news.js
@@ -1,38 +1,24 @@
 $(document).ready(function (e) {
-    $.bingSearch({
-        // Required: query text
-        query: 'query text here',
-        // Required (unless you use urlBase) by Bing Search API
-        appKey: '4ZMaudf3zFvWIeHmyO3P+k5a6IimVNUzrmMUOiLhVM4', 
-        // Optional (defaults to the Bing Search API Web Results Query).
-        // Additional information: This feature allows you to proxy through a server-side
-        //                         script in order to hide your API key, which is exposed to the
-        //                         world if you set it client-side in appKey. An example PHP
-        //                         script is included (searchproxy.php).
-
-        // Optional: Function is called after search results are retrieved, but before the interator is called
-        beforeSearchResults: function(data) {
-            // Use data.hasMore, data.resultBatchCount
-        },
-        // Function is called once per result in the current batch
-        searchResultIterator: function(data) {
-            // Use data.ID, data.Title, data.Description, data.Url, data.DisplayUrl, data.Metadata.Type (check for undefined)
-            var headlineLi = "<li>" + data.Title + "</li><li>|</li>"; 
-            $("#newsTicker").append(headlineLi);
-        },
-        // Optional: Function is called after search results are retrieved and after all instances of the interator are called
-        afterSearchResults: function(data) {
-            // Use data.hasMore, data.resultBatchCount
+    $.ajax({
+        method: 'GET',
+        url: 'https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=ba6c7724c1be423e9ef76de0ebf6cae3',
+        dataType: 'json',
+        success: function(data, stat) {
+            for(i = 0; i < data.articles.len; i++) {
+                var article = data.articles[i];
+                var headlineLi = "<li>" + article.title + "</li><li> | </li>"; 
+                $("#newsTicker").append(headlineLi);
+            }
             $("#newsTicker").endlessRiver({
                 speed: 50,
-		pause: true,
-		buttons: false
-            });
+                pause: true,
+                buttons: false
+            }); 
         },
-        // Optional: Called when there is an error retrieving results
-        fail: function(data) {
-            console.error("Error during bing search query:");
-            console.error(data);
+        error: function(stat, err) {
+            console.error('Error getting news: ' + err);
+            var headlineLi = "Cannot get news at this time.  Please refresh by [DOING SOME ACTION]"; 
+            $("#newsTicker").append(headlineLi);
         }
     });
 });
